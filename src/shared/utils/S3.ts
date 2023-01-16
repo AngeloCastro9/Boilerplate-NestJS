a86104c8d6f34fe6f154929f88f485ae2b7f2bd4
Angelo <castromatheus060@gmail.com>
2023-01-16 11:39:47 -0300

    feat(s3): new s3 version

--- src/shared/utils/S3.ts
+++ src/shared/utils/S3.ts
@@ -1,17 +1,41 @@
-import { S3 } from 'aws-sdk';
-import { randomBytes } from 'crypto';
+import { Injectable } from '@nestjs/common';
+import {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+  PutObjectCommandInput,
+} from '@aws-sdk/client-s3';
+@Injectable()
+export class S3Service {
+  private s3: S3Client;
+  constructor() {
+    this.s3 = new S3Client({
+      region: process.env.AWS_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+  }
 
-export default async function uploadPicS3(image: Buffer) {
-  const s3 = new S3();
-
-  const token = randomBytes(32).toString('hex');
-  const uploadResult = await s3
-    .upload({
+  async uploadPicS3(image: Buffer, token: string, mimetype: string) {
+    const uploadParams = {
       Bucket: process.env.AWS_PUBLIC_BUCKET_NAME,
+      Key: `${token}`,
       Body: image,
-      Key: token,
-    })
-    .promise();
+      ContentType: mimetype,
+    } as PutObjectCommandInput;
 
-  return uploadResult.Location
+    await this.s3.send(new PutObjectCommand(uploadParams));
+    return `https://${process.env.AWS_PUBLIC_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${token}`;
+  }
+
+  async deletePicS3(key: string) {
+    const deleteParams = {
+      Bucket: process.env.AWS_PUBLIC_BUCKET_NAME,
+      Key: key,
+    };
+
+    return this.s3.send(new DeleteObjectCommand(deleteParams));
+  }
 }
