ea2ca31d6fa5d0def52d6aaaf915bde285dba7b3
Angelo <castromatheus060@gmail.com>
2023-01-16 11:40:15 -0300

    feat(admin): Update login to new prisma version

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,138 +1,114 @@
-import { BadRequestException, Injectable, NotAcceptableException } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  Injectable,
+  NotAcceptableException,
+} from '@nestjs/common';
 import { admin, Prisma } from '@prisma/client';
 import { PrismaService } from '../database/prisma.service';
 import { MailService } from '../mail/service/mail.service';
 
 @Injectable()
 export class AdminService {
-    constructor(
-        private readonly prisma: PrismaService,
-        private readonly mail: MailService,
-    ) { }
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly mail: MailService,
+  ) {}
 
-    async create(data: Prisma.adminCreateInput): Promise<admin> {
-        const admin = await this.prisma.admin.findUnique({
-            where: {
-                email: data.email,
-            },
-        });
+  async create(data: Prisma.adminCreateInput): Promise<admin> {
+    const admin = await this.prisma.admin.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
 
-        if (admin) {
-            throw new BadRequestException(
-                'User already exists',
-            );
-        }
-
-        const adminCreated = await this.prisma.admin.create({ data });
-
-        await this.mail.sendWelcome(data.email, data.name);
-
-        delete adminCreated.password;
-
-        return adminCreated;
+    if (admin) {
+      throw new BadRequestException('User already exists');
     }
 
-    async readOne(id: number): Promise<admin> {
-        const admin = await this.prisma.admin.findUnique({
-            where: {
-                id
-            },
-        });
+    const adminCreated = await this.prisma.admin.create({ data });
 
-        if (!admin) {
-            throw new BadRequestException(
-                'User already exists',
-            );
-        }
+    await this.mail.sendWelcome(data.email, data.name);
 
-        delete admin.password;
-        delete admin.resetPassCode;
+    delete adminCreated.password;
 
-        return admin;
+    return adminCreated;
+  }
+
+  async readOne(id: number): Promise<admin> {
+    try {
+      return this.prisma.admin.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      throw new HttpException(error, 400);
     }
+  }
 
-    async update(data: Prisma.adminUpdateInput, id: number): Promise<admin> {
-        const admin = await this.prisma.admin.findUnique({
-            where: {
-                id
-            },
-        });
+  async update(data: Prisma.adminUpdateInput, id: number): Promise<admin> {
+    try {
+      await this.prisma.admin.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      });
 
-        if (!admin) {
-            throw new BadRequestException(
-                'User not found',
-            );
-        }
-
-        const adminUpdated = await this.prisma.admin.update({
-            where: {
-                id
-            },
-            data
-        });
-
-        delete adminUpdated.password;
-
-        return adminUpdated;
+      return this.prisma.admin.update({
+        where: {
+          id,
+        },
+        data,
+      });
+    } catch (error) {
+      throw new HttpException(error, 400);
     }
+  }
 
-    async disable(id: number): Promise<admin> {
-        const admin = await this.prisma.admin.findUnique({
-            where: {
-                id
-            },
-        });
+  async disable(id: number): Promise<admin> {
+    try {
+      await this.prisma.admin.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      });
 
-        if (!admin) {
-            throw new BadRequestException(
-                'User not found',
-            );
-        }
-
-        const adminDisabled = await this.prisma.admin.update({
-            where: {
-                id
-            },
-            data: {
-                status: 'Block'
-            }
-        });
-
-        delete adminDisabled.password;
-
-        return adminDisabled;
+      return this.prisma.admin.update({
+        where: {
+          id,
+        },
+        data: {
+          status: 'Block',
+        },
+      });
+    } catch (error) {
+      throw new HttpException(error, 400);
     }
+  }
 
-    async enable(id: number): Promise<admin> {
-        const admin = await this.prisma.admin.findUnique({
-            where: {
-                id
-            },
-        });
+  async enable(id: number): Promise<admin> {
+    try {
+      const admin = await this.prisma.admin.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      });
 
-        if (!admin) {
-            throw new BadRequestException(
-                'Admin not found',
-            );
-        }
+      if (admin.status === 'Activate') {
+        throw new NotAcceptableException('Admin already activate');
+      }
 
-        if (admin.status === 'Activate') {
-            throw new NotAcceptableException(
-                'Admin already activate'
-            )
-        }
-
-        const adminDisabled = await this.prisma.admin.update({
-            where: {
-                id
-            },
-            data: {
-                status: 'Activate'
-            }
-        });
-
-        delete adminDisabled.password;
-
-        return adminDisabled;
+      return this.prisma.admin.update({
+        where: {
+          id,
+        },
+        data: {
+          status: 'Activate',
+        },
+      });
+    } catch (error) {
+      throw new HttpException(error, 400);
     }
+  }
 }
